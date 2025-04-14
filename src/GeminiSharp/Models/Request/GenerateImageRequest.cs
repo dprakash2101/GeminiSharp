434ccc94bc8e15165515eeac76887e706a511e2f
dprakash2101 <dprakash2101@gmail.com>
2025-04-14 20:16:59 +0530

    Updated model

--- src/GeminiSharp/Models/Request/GenerateImageRequest.cs
+++ src/GeminiSharp/Models/Request/GenerateImageRequest.cs
@@ -1,13 +1,18 @@
-﻿namespace GeminiSharp.Models.Request
+﻿using System.Text.Json.Serialization;
+
+namespace GeminiSharp.Models.Request
 {
     public class GenerateImageRequest
     {
+        [JsonPropertyName("contents")]
         public List<RequestContent> Contents { get; set; } = new();
+        [JsonPropertyName("generationConfig")]
         public ImageGenerationConfig? generationConfig { get; set; } 
 
     }
     public class ImageGenerationConfig
     {
+        [JsonPropertyName("responseModalities")]
         public List<string>? ResponseModalities { get; set; }
     }
 }
